a1e0b1eb520c086c6ba01beaa295206b75c12403
Mayank Mandava <mayankmandava@gmail.com>
2022-06-01 08:23:08 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,49 @@
+var express = require('express');
+var cors = require('cors'); // Use cors module for enable Cross-origin resource sharing
+const fs = require('fs')
+const filename = "./count.json"
+
+var app = express();
+app.use(cors()); // for all routes
+
+var port = process.env.PORT || 8080;
+
+function reset(callback) {
+	fs.writeFile(filename, JSON.stringify({count: 0}),() => {callback({count: 0})})
+}
+
+function updatecount(by, callback) {
+	fs.readFile(filename, 'utf-8', (err, data) => {
+		if(err) {
+			console.log(err)
+			return
+		}
+		let {count} = JSON.parse(data)
+		count = count + by
+		fs.writeFile(filename, JSON.stringify({count}),() => {callback({count})})
+	})
+}
+
+function getcount() {
+	const {count} = JSON.parse(fs.readFileSync(filename, 'utf-8'))
+	return count
+}
+
+app.get('/', function(req, res) {
+	const count = getcount()
+	res.json({count})
+})
+
+app.post('/:count', function(req, res) {
+	const {count} = req.params
+	updatecount(parseInt(parseInt(count)), ({count}) => res.json({count}) )
+})
+
+app.post('/reset', function(req, res) {
+	reset(({count}) => res.json({count}) )
+})
+
+
+app.listen(port, function() {
+    console.log('Node.js listening on port ' + port)
+})
