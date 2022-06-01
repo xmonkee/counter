6480ac3f506a3451a60314b7e807ea07100417f0
Mayank Mandava <mayankmandava@gmail.com>
2022-06-01 12:30:21 -0700

    Fix things

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+#!env node
 var express = require('express');
 var cors = require('cors'); // Use cors module for enable Cross-origin resource sharing
 const fs = require('fs')
@@ -7,10 +8,6 @@ var app = express();
 app.use(cors()); // for all routes
 
 var port = process.env.PORT || 8080;
-
-function reset(callback) {
-	fs.writeFile(filename, JSON.stringify({count: 0}),() => {callback({count: 0})})
-}
 
 function updatecount(by, callback) {
 	fs.readFile(filename, 'utf-8', (err, data) => {
@@ -29,7 +26,8 @@ function getcount() {
 	return count
 }
 
-app.get('/', function(req, res) {
+app.post('/reset', function(req, res) {
+	fs.writeFileSync(filename, '{"count":0}', 'utf-8')
 	const count = getcount()
 	res.json({count})
 })
@@ -39,10 +37,10 @@ app.post('/:count', function(req, res) {
 	updatecount(parseInt(parseInt(count)), ({count}) => res.json({count}) )
 })
 
-app.post('/reset', function(req, res) {
-	reset(({count}) => res.json({count}) )
+app.get('/', function(req, res) {
+	const count = getcount()
+	res.json({count})
 })
-
 
 app.listen(port, function() {
     console.log('Node.js listening on port ' + port)
